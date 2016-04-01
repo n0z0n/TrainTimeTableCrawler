71cea18c038d2a62f2e05705fc893f7a5c2e0018
n0z0n <mail+github@n0z.me>
2016-04-01 15:58:50 +0900

    Create View-Model Base class.

---
+++ Solutions/TimeTableCrawler/Commons/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿namespace TimeTableCrawler.Commons
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using TimeTableCrawler.Annotations;
+
+    public class ViewModelBase : INotifyPropertyChanged, IDataErrorInfo
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Error { get; private set; }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- Solutions/TimeTableCrawler/MainForm.Designer.cs
+++ Solutions/TimeTableCrawler/MainForm.Designer.cs
@@ -28,12 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 701);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
     }
 }
 
